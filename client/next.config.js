f2870a9449d4f33bfb3b155abb50e9aa4a73f22c
harsh-100 <26harshagarwal@gmail.com>
2023-07-05 22:23:43 +0530

    changes in setup

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
+const nextConfig = {
+  // Tell Next.js to look for pages
+  // in the `src` directory
+  pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
+  webpack: (config, { isServer }) => {
+    // Move the styles to the `src` directory
+    if (!isServer) {
+      config.resolve.alias["@styles"] = path.resolve(__dirname, "src/styles");
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
